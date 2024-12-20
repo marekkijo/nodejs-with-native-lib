2e5a960e34d28f4a695aacd6c5185509cea40da7
Marek Kijo <marekkijo@gmail.com>
2024-12-20 08:18:23 +0100

    10: Handle callback from native to JS

--- native_lib/src/native_lib/native_lib.cpp
+++ native_lib/src/native_lib/native_lib.cpp
@@ -1,16 +1,30 @@
 #include "native_lib/native_lib.hpp"
 
+#include <functional>
 #include <iostream>
 #include <string>
 
 namespace {
 std::string message = "Hello from native_lib!";
+std::function<int(int)> callback;
 } // namespace
 
 void set_message(const char *new_message) { message = new_message; }
 
 const char *get_message() { return message.c_str(); }
 
-void print_message() { std::cout << "Message: " << message << std::endl; }
+void print_message() { std::cout << "print_message: " << message << std::endl; }
 
-void print_int(const int value) { std::cout << "Value: " << value << std::endl; }
+void print_int(const int value) { std::cout << "print_int: " << value << std::endl; }
+
+void register_callback(int (*cb)(int)) { callback = cb; }
+
+void call_callback(int value) {
+  std::cout << "call_callback: " << value << std::endl;
+  if (callback) {
+    int result = callback(value * value);
+    std::cout << "Callback result: " << result << std::endl;
+  } else {
+    std::cerr << "Callback is not set!" << std::endl;
+  }
+}
