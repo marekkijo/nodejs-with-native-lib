8a488ecc01aca2839a3289862eaddcf7f71f32e1
Marek Kijo <marekkijo@gmail.com>
2024-12-20 02:01:57 +0100

    5: Try native basic example of Koffi

---
+++ nodejs_app/koffi_get_started.js
@@ -0,0 +1,21 @@
+const koffi = require('koffi');
+
+const lib = koffi.load('user32.dll');
+
+// Declare constants
+const MB_OK = 0x0;
+const MB_YESNO = 0x4;
+const MB_ICONQUESTION = 0x20;
+const MB_ICONINFORMATION = 0x40;
+const IDOK = 1;
+const IDYES = 6;
+const IDNO = 7;
+
+// Find functions
+const MessageBoxA = lib.func('__stdcall', 'MessageBoxA', 'int', ['void *', 'str', 'str', 'uint']);
+const MessageBoxW = lib.func('__stdcall', 'MessageBoxW', 'int', ['void *', 'str16', 'str16', 'uint']);
+
+let ret = MessageBoxA(null, 'Do you want another message box?', 'Koffi', MB_YESNO | MB_ICONQUESTION);
+
+if (ret == IDYES)
+    MessageBoxW(null, 'Hello World!', 'Koffi', MB_ICONINFORMATION);
